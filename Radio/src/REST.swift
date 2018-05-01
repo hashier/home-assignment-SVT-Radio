1a70ca2ccc38aba4e3b061c7d20b371d96f93052
Christopher Loessl <cloessl+github@gmail.com>
2018-05-01 19:38:11 +0200

    Add: Structure

---
+++ Radio/src/REST.swift
@@ -0,0 +1,9 @@
+//
+//  REST.swift
+//  Radio
+//
+//  Created by Christopher Lössl on 2018-05-01.
+//  Copyright © 2018 Christopher Lössl. All rights reserved.
+//
+
+import Foundation
---
+++ Radio/src/Result.swift
@@ -0,0 +1,9 @@
+//
+//  Result.swift
+//  Radio
+//
+//  Created by Christopher Lössl on 2018-05-01.
+//  Copyright © 2018 Christopher Lössl. All rights reserved.
+//
+
+import Foundation
